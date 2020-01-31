4b81485a94f9cd8deb16d50a021d7e749299f9c7
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-31 22:10:38 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,13 +1,2 @@
 require "pry"
 
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-end
-
-binding.pry
-
-hello(["Tim","Tom", "Jim"]) {|name| puts "Hi, #{name}" }
